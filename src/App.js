bd45bb502fb7b96af221a7f9342d0149a0a44d78
Heiko <hfrosch@gmail.com>
2020-12-09 12:56:49 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Link, Switch, Route, useParams } from "react-router-dom";
 
-function App() {
+const PokemonList = () => {
+  const [pokemon, setPokemon] = useState([]);
+  useEffect(() => {
+    fetch("https://pokefight-api.herokuapp.com/pokemon")
+      .then((res) => res.json())
+      .then((res) => setPokemon(res));
+  }, []);
+  return (
+    <>
+      {pokemon.map((p) => (
+        <li>
+          <Link to={`/pokemon/${p.id}`}>{p.name.english}</Link>
+        </li>
+      ))}
+    </>
+  );
+};
+
+const PokemonSingle = ({ pokemon }) => {
+  const { id } = useParams();
+  const [matchedPokemon, setMatchedPokemon] = useState();
+  useEffect(() => {
+    fetch(`https://pokefight-api.herokuapp.com/pokemon/${id}`)
+      .then((res) => res.json())
+      .then((res) => setMatchedPokemon(res));
+  }, []);
+  return matchedPokemon ? (
+    <div className="detail">
+      <h1>{matchedPokemon.name.english}</h1>
+      <h2>ID: {matchedPokemon.id}</h2>
+      <p>
+        <b>Type: </b>
+        {matchedPokemon.type}
+        <br />
+      </p>
+      <p>
+        <b>HP: </b>
+        {matchedPokemon.base.HP}
+      </p>
+      <p>
+        <b>Attack: </b>
+        {matchedPokemon.base.Attack}
+      </p>
+      <p>
+        <b>Defense: </b>
+        {matchedPokemon.base.Defense}
+      </p>
+      <p>
+        <b>Speed: </b>
+        {matchedPokemon.base.Speed}
+      </p>
+    </div>
+  ) : (
+    <>loading.....</>
+  );
+};
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/">
+          <PokemonList />
+        </Route>
+        <Route exact path="/pokemon/:id">
+          <PokemonSingle />
+        </Route>
+        <Route exact path="/pokemon/:id/:info"></Route>
+      </Switch>
     </div>
   );
 }
-
-export default App;
